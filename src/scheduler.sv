52143040d0045c337163aef2311afcdd33ee1623
Ayandeep Hazra <ahazra2@wisc.edu>
2024-04-24 07:52:26 -0500

    init tb
    changing word format to suit readability

--- src/scheduler.sv
+++ src/scheduler.sv
@@ -21,6 +21,8 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 	// local parameters
 	
 	parameter INST_INDEX_MSB = 19+3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA);
+	parameter START_TIME_MSB = 17;
+	parameter START_TIME_LSB = 2;
 	
 	wire [1:0] status_wire;
 	wire [15:0] start_time;
@@ -31,24 +33,27 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 	
 	// Register Microarchitecture
 	
-	reg [0:NUM_REGS_L1-1] [3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)+20:0] regs_layer1;
-	reg [0:NUM_REGS_L2-1] [3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)+20:0] regs_layer2;
+	reg [0:NUM_REGS_L1-1] [INST_INDEX_MSB:0] regs_layer1;
+	reg [0:NUM_REGS_L2-1] [INST_INDEX_MSB:0] regs_layer2;
 	reg [15:0] curr_timestamp;
 	
 	// first layer of registers
 	
 	int i;
 	
-	
 	always_ff @ (posedge clk, negedge rst_n) begin
-		if (!rst_n)
+		if (!rst_n) begin
 			ireg_index <= 0;
-		else if (ireg_index == NUM_INSTRS-1)
+			for(i = 0; i<NUM_REGS_L1; i++)
+				regs_layer1[i] <= 0;
+		end
+		else if (ireg_index >= NUM_INSTRS-1)
 			ireg_index <= 0;
 		
 		for(i = 0; i<NUM_REGS_L1; i++)
 			if (~regs_layer1[i][1] & instruction[ireg_index][1]) begin
-				regs_layer1[i] <= {instruction[ireg_index++][INST_INDEX_MSB:2],2'b10};
+				status[ireg_index] = 0;
+				regs_layer1[i] <= {instruction[ireg_index++][INST_INDEX_MSB:START_TIME_LSB],2'b10};
 			end
 	end
 	
@@ -57,7 +62,35 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 	// 1. check what the current dominant timestamp is
 	// 2. fill from first layers any available instructions with matching timestamp
 	
+	int j;
+	reg [$clog2(NUM_REGS_L1)-1:0] lreg_index; // index of l1 being read from
+	reg [$clog2(NUM_REGS_L1)-1:0] lreg_population;
+	
 	always_ff @ (posedge clk, negedge rst_n) begin
+	
+		if (!rst_n) begin
+			lreg_index <= 0;
+			lreg_population <= 0;
+			curr_timestamp <= 0;
+			for(i = 0; i<NUM_REGS_L2; i++)
+				regs_layer1[i] <= 0;
+			end
+		else if (lreg_index >= NUM_REGS_L1-1)
+			lreg_index <= 0;
+			
+		/* case where l2 has no active instructions */
+		
+		if(curr_timestamp==16'h0000) begin
+			curr_timestamp <= regs_layer1[lreg_index][START_TIME_MSB:START_TIME_LSB];
+		end
+		
+		/* case where l2 has active instructions */
+	
+		for(j = 0; j<NUM_REGS_L2; j++)
+			if (~regs_layer2[j][1] & (curr_timestamp == regs_layer1[lreg_index][START_TIME_MSB:START_TIME_LSB]) ) begin
+				regs_layer2[j] <= {regs_layer1[lreg_index++][INST_INDEX_MSB:START_TIME_LSB],2'b10};
+				lreg_population++;
+			end
 	
 	end
 	

219f42ba5c6c213bf381cc2b346077fbb1f18fd9
Ayandeep Hazra <ahazra2@wisc.edu>
2024-04-24 08:45:12 -0500

    basic fill functionality works

--- src/scheduler.sv
+++ src/scheduler.sv
@@ -4,38 +4,32 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 		input clk,
 		input rst_n,
 		input reg [0:NUM_INSTRS-1] 
-			[3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)+19:0] instruction,	// from I CACHE
+			[3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)+21:0] instruction,	// from I CACHE
 		output reg [NUM_INSTRS-1:0] status,
 		output reg [$clog2(NUM_INSTRS)-1:0] ireg_index					// where to fill up until
 	);
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
-	//  I-WORD = < op_code 		19+3*clog(total qubits):18+3*clog(total qubits)
-	//           | op_1 		17+3*clog(total qubits):18+2*clog(total qubits)
-	//			 | op_2 		17+2*clog(total qubits):18+clog(total qubits)
-	//		     | dest 		17+clog(total qubits):18 
-	// 		     | start_time 	17:2 
-	//		  	 | status 		1:0  >                                                                
+	//  I-WORD = < op_code 		21+3*clog(total qubits):20+3*clog(total qubits)
+	//           | op_1 		19+3*clog(total qubits):20+2*clog(total qubits)
+	//			 | op_2 		19+2*clog(total qubits):20+clog(total qubits)
+	//		     | dest 		19+clog(total qubits):20 
+	// 		     | start_time 	19:4 
+	//		  	 | status 		3:0  >                                                                
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	// local parameters
 	
-	parameter INST_INDEX_MSB = 19+3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA);
-	parameter START_TIME_MSB = 17;
-	parameter START_TIME_LSB = 2;
-	
-	wire [1:0] status_wire;
-	wire [15:0] start_time;
-	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] dest;
-	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] op_1;
-	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] op_2;
-	wire [1:0] op_code; 									// 4 total so 2 bits
+	parameter INST_INDEX_MSB = 21+3*$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA);
+	parameter START_TIME_MSB = 19;
+	parameter START_TIME_LSB = 4;
+
 	
 	// Register Microarchitecture
 	
 	reg [0:NUM_REGS_L1-1] [INST_INDEX_MSB:0] regs_layer1;
 	reg [0:NUM_REGS_L2-1] [INST_INDEX_MSB:0] regs_layer2;
-	reg [15:0] curr_timestamp;
+	reg [START_TIME_MSB:START_TIME_LSB] curr_timestamp;
 	
 	// first layer of registers
 	
@@ -51,9 +45,9 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 			ireg_index <= 0;
 		
 		for(i = 0; i<NUM_REGS_L1; i++)
-			if (~regs_layer1[i][1] & instruction[ireg_index][1]) begin
+			if (~regs_layer1[i][3] & instruction[ireg_index][3]) begin
 				status[ireg_index] = 0;
-				regs_layer1[i] <= {instruction[ireg_index++][INST_INDEX_MSB:START_TIME_LSB],2'b10};
+				regs_layer1[i] <= {instruction[ireg_index++][INST_INDEX_MSB:START_TIME_LSB],4'b1000};
 			end
 	end
 	
@@ -73,7 +67,7 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 			lreg_population <= 0;
 			curr_timestamp <= 0;
 			for(i = 0; i<NUM_REGS_L2; i++)
-				regs_layer1[i] <= 0;
+				regs_layer2[i] <= 0;
 			end
 		else if (lreg_index >= NUM_REGS_L1-1)
 			lreg_index <= 0;
@@ -85,15 +79,27 @@ module scheduler # (parameter DEPTH = 64, NUM_FPGA = 64, NUM_QUBIT_PER_FPGA = 64
 		end
 		
 		/* case where l2 has active instructions */
-	
-		for(j = 0; j<NUM_REGS_L2; j++)
-			if (~regs_layer2[j][1] & (curr_timestamp == regs_layer1[lreg_index][START_TIME_MSB:START_TIME_LSB]) ) begin
-				regs_layer2[j] <= {regs_layer1[lreg_index++][INST_INDEX_MSB:START_TIME_LSB],2'b10};
-				lreg_population++;
-			end
-	
+		else begin
+			for(j = 0; j<NUM_REGS_L2; j++)
+				if (~regs_layer2[j][3] & (curr_timestamp == regs_layer1[lreg_index][START_TIME_MSB:START_TIME_LSB]) ) begin
+					regs_layer2[j] <= {regs_layer1[lreg_index++][INST_INDEX_MSB:START_TIME_LSB],4'b1000};
+					lreg_population++;
+				end
+		end
 	end
 	
 	// functional units
 	
 endmodule
+
+/*
+
+	
+	wire [1:0] status_wire;
+	wire [15:0] start_time;
+	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] dest;
+	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] op_1;
+	wire [$clog2(NUM_FPGA*NUM_QUBIT_PER_FPGA)-1:0] op_2;
+	wire [1:0] op_code; 									// 4 total so 2 bits
+	
+	*/
